6165ecbec29bfb2b84c6f68967673a731059881d
rajar <rajarajeshwarir2021@gmail.com>
2024-01-26 13:13:23 +0100

    mlops_water_potability_prediction_project/__init__.py

---
+++ src/mlops_water_potability_prediction_project/__init__.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import logging
+
+LOG_DIRECTORY = "logs"
+LOGGING_STRING = "[%(asctime)s]: %(levelname)s: %(module)s: %(message)s]"
+
+LOG_FILE_PATH = os.path.join(LOG_DIRECTORY,"run_logs.log")
+os.makedirs(LOG_FILE_PATH, exist_ok=True)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOGGING_STRING,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("mlops_water_potability_prediction_project_logger")
