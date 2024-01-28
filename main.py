be7a8943f3943d38fd5c0fba618a2a2f6124f5be
rajar <rajarajeshwarir2021@gmail.com>
2024-01-28 20:50:55 +0100

    Stage 3: Completed

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from src.mlops_water_potability_prediction_project import logger
 from src.mlops_water_potability_prediction_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlops_water_potability_prediction_project.pipeline.stage_02_data_validation import \
+    DataValidationTrainingPipeline
 
-STAGE_NAME = "DATA INGESTION"
+# STAGE_NAME = "DATA INGESTION"
+# try:
+#     logger.info(f">>>>>> STAGE: {STAGE_NAME} started <<<<<<")
+#     DataIngestionTrainingPipeline().ingest_data()
+#     logger.info(f">>>>>> STAGE: {STAGE_NAME} completed <<<<<<\n\nX==========X")
+# except Exception as e:
+#     logger.error(f"Error during overall execution: {e}")
+#     raise e
 
 
+STAGE_NAME = "DATA VALIDATION"
 try:
     logger.info(f">>>>>> STAGE: {STAGE_NAME} started <<<<<<")
-    DataIngestionTrainingPipeline().main()
+    DataValidationTrainingPipeline().validate_data()
     logger.info(f">>>>>> STAGE: {STAGE_NAME} completed <<<<<<\n\nX==========X")
 except Exception as e:
     logger.error(f"Error during overall execution: {e}")
