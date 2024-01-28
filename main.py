1edde41203e0d461f3650f4ad69e49c2400d74c1
rajar <rajarajeshwarir2021@gmail.com>
2024-01-28 18:10:29 +0100

    Stage 1: Completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.mlops_water_potability_prediction_project import logger
+from src.mlops_water_potability_prediction_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello World")
+STAGE_NAME = "DATA INGESTION"
+
+
+try:
+    logger.info(f">>>>>> STAGE: {STAGE_NAME} started <<<<<<")
+    DataIngestionTrainingPipeline().main()
+    logger.info(f">>>>>> STAGE: {STAGE_NAME} completed <<<<<<\n\nX==========X")
+except Exception as e:
+    logger.error(f"Error during overall execution: {e}")
+    raise e
