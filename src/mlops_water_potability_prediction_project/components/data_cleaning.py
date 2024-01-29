43539d9742676b8bd683d46c01be956f341de25d
rajar <rajarajeshwarir2021@gmail.com>
2024-01-29 10:43:00 +0100

    Data Cleaning: Created and implemented data_cleaning.py

---
+++ src/mlops_water_potability_prediction_project/components/data_cleaning.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from src.mlops_water_potability_prediction_project import logger
+from src.mlops_water_potability_prediction_project.entity.config_entity import DataCleaningConfig
+
+
+class DataCleaning:
+    """
+    DataCleaning class for cleaning data.
+
+    Attributes:
+        config (DataCleaningConfig): The configuration for data cleaning.
+
+    Methods:
+        clean_data(): Reads unclean data, removes any rows with missing values, and saves the cleaned data.
+
+    Note:
+        This class assumes the use of a logger instance from the 'src.mlops_water_potability_prediction_project' module.
+    """
+
+    def __init__(self, config: DataCleaningConfig):
+        """
+        Initialize DataCleaning instance.
+
+        Args:
+            config (DataCleaningConfig): The configuration for data cleaning.
+        """
+        self.config = config
+
+    def clean_data(self):
+        """
+        Clean the data by removing rows with missing values.
+
+        Raises:
+            Exception: If an error occurs during the cleaning process.
+        """
+        try:
+            # Read the unclean data
+            dataframe = pd.read_csv(self.config.unclean_data_path)
+
+            # Remove rows with any missing values
+            dataframe = dataframe.dropna(how='any', axis=0)
+
+            # Save the cleaned data
+            dataframe.to_csv(self.config.clean_data_path, index=False)
+
+            # Log information about the cleaning process
+            logger.info("Cleaned the dataset")
+
+        except Exception as e:
+            # Raise an exception if an error occurs during cleaning
+            raise e
