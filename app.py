55376218b2fd9f56e0c0547fcad74d366e8052c4
rajar <rajarajeshwarir2021@gmail.com>
2024-01-30 15:54:43 +0100

    Stage 8: Completed

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-import os
 from flask import Flask, render_template, request, jsonify
 
-webapp_root ="webapp"
+from src.mlops_water_potability_prediction_project.components.frontend import FrontendPrediction
+from src.mlops_water_potability_prediction_project.config.configuration import ConfigurationManager
 
-static_dir = os.path.join(webapp_root, "static")
-templates_dir = os.path.join(webapp_root, "templates")
+# Assuming 'data' is a dictionary containing input values
+SAMPLE_DATA = {
+    'ph': '2',
+    'Hardness': '75',
+    'Solids': '350',
+    'Chloramines': '2',
+    'Sulfate': '150',
+    'Conductivity': '250',
+    'Organic_carbon': '10',
+    'Trihalomethanes': '10',
+    'Turbidity': '5'
+}
+
+# Create a configuration manager instance
+config = ConfigurationManager()
+frontend_config = config.get_frontend_config()
+
+static_dir = frontend_config.static_dir
+templates_dir = frontend_config.template_dir
 
 app = Flask(__name__, static_folder=static_dir, template_folder=templates_dir)
 
@@ -15,13 +32,10 @@ def index():
         try:
             if request.form:
                 data_req = dict(request.form)
-                response = prediction.form_response(data_req)
+                frontend_predictor = FrontendPrediction(config=frontend_config)
+                response = frontend_predictor.form_response(data_req)
+                print(response)
                 return render_template('index.html', response=response)
-
-            elif request.json:
-                response = prediction.api_response(request.json)
-                return jsonify(response)
-
         except Exception as e:
             print(e)
             error_message = {"error": e}
